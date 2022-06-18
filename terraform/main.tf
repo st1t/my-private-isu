3db650d0d7ba67a6b7cc6fab2da48f289b4113e5
Shota Ito <shota.ito.jp@gmail.com>
2022-06-18 18:46:02 +0900

    isucon11予選用にterraformを修正

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # localesに登録しているものは全て個々の事情に応じて修正してOK
 # AMIを変えれば過去のISUCON環境も作れる。 https://github.com/matsuu/aws-isucon#ami
 locals {
-  app_name      = "${local.github_user}-private-isu" # あらゆるリソースのタグに利用している。好きな名前に修正してOK
+  app_name      = "${local.github_user}-isucon11-q" # あらゆるリソースのタグに利用している。好きな名前に修正してOK
   cidr_vpc      = "10.0"                             # Networkの第1,2オクテットまでを指定。既存のVPCとCIDRを被せたくない時に修正すると良い
   subnet_name   = "${local.app_name}-public-a"       # EC2のsubnet。末尾のa,c,dを変えればそれぞれのAZに構築される
   github_user   = "st1t"                             # GitHubに公開されている公開鍵を使ってAWSのキーペアに利用。GitHubに複数の鍵が登録している場合はどの鍵が利用されているか注意
@@ -9,12 +9,12 @@ locals {
   os_login_user = "ubuntu"                           # authorized_keysを設定するOSのユーザー名
 
   game_instance_count = "1"                     # 競技者用EC2インスタンスの台数
-  game_instance_ami   = "ami-0b37d5c92add6d0d5" # 競技者用EC2インスタンスのAMI
+  game_instance_ami   = "ami-0796be4f4814fc3d5" # 競技者用EC2インスタンスのAMI
   game_instance_type  = "c6i.large"             # 競技者用EC2インスタンスタイプ。公式推奨はc4.large
   game_spot_price     = "0.1"                   # 競技者用EC2スポットインスタンス料金。同一インスタンスタイプでもAZによって価格が異なるので注意
 
-  bench_instance_count = "1"                     # ベンマーク用EC2インスタンスの台数
-  bench_instance_ami   = "ami-024cfcacc753fa53e" # ベンチマーク用EC2インスタンスのAMI
+  bench_instance_count = "0"                     # ベンマーク用EC2インスタンスの台数
+  bench_instance_ami   = "ami-0796be4f4814fc3d5" # ベンチマーク用EC2インスタンスのAMI
   bench_instance_type  = "c6i.xlarge"            # ベンチマーク用EC2インスタンスタイプ。公式推奨はc5.xlarge
   bench_spot_price     = "0.1"                   # ベンチマーク用EC2スポットインスタンスの最高価格。
 }

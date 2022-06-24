c802280c2d4033dd1a16ee0a500afcf8abbc9e73
Shota Ito <shota.ito.jp@gmail.com>
2022-06-24 16:18:32 +0900

    GET /api/trendを改善

--- server-files/home/isucon/webapp/ruby/app.rb
+++ server-files/home/isucon/webapp/ruby/app.rb
@@ -583,13 +583,13 @@ module Isucondition
       character_list = db.query('SELECT `character` FROM `isu` GROUP BY `character`')
 
       res = character_list.map do |character|
-        isu_list = db.xquery('SELECT * FROM `isu` WHERE `character` = ?', character.fetch(:character))
+        isu_list = db.xquery('SELECT id,jia_isu_uuid FROM `isu` WHERE `character` = ?', character.fetch(:character))
         character_info_isu_conditions = []
         character_warning_isu_conditions = []
         character_critical_isu_conditions = []
 
         isu_list.each do |isu|
-          conditions = db.xquery('SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC', isu.fetch(:jia_isu_uuid)).to_a
+          conditions = db.xquery('SELECT `condition`,`timestamp` FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC LIMIT 1', isu.fetch(:jia_isu_uuid)).to_a
           unless conditions.empty?
             isu_last_condition = conditions.first
             condition_level = calculate_condition_level(isu_last_condition.fetch(:condition))
@@ -605,9 +605,9 @@ module Isucondition
           end
         end
 
-        character_info_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
-        character_warning_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
-        character_critical_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
+        # character_info_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
+        # character_warning_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
+        # character_critical_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
 
         {
           character: character.fetch(:character),
